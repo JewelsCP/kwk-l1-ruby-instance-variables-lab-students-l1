cb8cad7a0c31774d42fd8ccc6bca8bcf957da461
JewelsCP <giulianacarballoparker@gmail.com>
2018-06-08 19:53:38 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,16 +1,16 @@
 # dog.rb
-class Dog
-  def name (dog_name)
-    @this_dogs_name = dog_name
-  end
+# class Dog
+#   def name (dog_name)
+#     @this_dogs_name = dog_name
+#   end
  
-  def name
-    @this_dogs_name
-  end
+#   def name
+#     @this_dogs_name
+#   end
 
-end
+# end
 
-lassie = Dog.new
-lassie.name = "Lassie"
+# lassie = Dog.new
+# lassie.name = "Lassie"
  
-puts lassie.name
+# puts lassie.name
